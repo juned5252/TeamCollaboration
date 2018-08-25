a35c4d812282d47e1cf18e789f52e23504f5c1a6
Juned Alam <juned5252@gmail.com>
2018-08-25 19:17:11 -0400

    update

--- src/TestCalc.java
+++ src/TestCalc.java
@@ -1,5 +1,13 @@
 public class TestCalc {
     public static void main(String[] args) {
 
+
+
+        String s="123456789";
+        System.out.println( s.length());
+
+        int[] arr = new int[10];
+        System.out.println(arr.length);
+
     }
 }
