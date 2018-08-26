16d388c935035176f769b7ef1ac2a5ad7dc6f611
almrashid <alrashidm@outlook.com>
2018-08-25 20:00:57 -0400

    chagned made as requested

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,10 +10,12 @@ public class Calculator {
     public static int sub(int x, int y){
         return x-y;
     }
-    public static int div(int x, int y){
-        return x/y;
+    public static int div(int x, int y) {
+        return x / y;
     }
-
+        public static double div(int x, double y){
+            return x/y;
+        }
 
 
 }
