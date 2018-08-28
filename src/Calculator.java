99a6443639d783fdcf920e594a21135c8d1b7547
Juned Alam <juned5252@gmail.com>
2018-08-28 10:18:37 -0400

    added reverse String method

--- src/Calculator.java
+++ src/Calculator.java
@@ -31,6 +31,14 @@ public class Calculator {
         return Math.pow(a,b);
     }
 
+    public String reverse(String str) {
+        if (str.length() <= 1) {
+            return str;
+        }
+        return str.charAt(str.length() - 1) +
+                reverse(str.substring(0, str.length() - 1));
+    }
+
 
 
 
