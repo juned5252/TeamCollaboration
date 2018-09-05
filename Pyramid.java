7467b05a93035b01de94572afbae6bb993140dff
jmintegr1 <jmintegr1@yahoo.com>
2018-09-05 10:11:07 -0400

    Pyramid

---
+++ Pyramid.java
@@ -0,0 +1,21 @@
+package egypt;
+
+public class Pyramid {
+
+    public static void main(String[] args) {
+        makePyramid(5);
+    }
+
+    public static void makePyramid(int max){
+        for (int i = 1; i<max; i+=1){
+            for(int j=i; j<max-1; j++){
+                System.out.print(" ");
+            }
+            for(int j = i; j>0; j--){
+                System.out.print(i + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
